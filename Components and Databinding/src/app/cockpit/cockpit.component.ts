b1ae7eef6af117412a5cb03799bc5ceb2e60e1b7
jose-guerrero <jose.guerrerog@outlook.com>
2018-05-20 19:17:50 +1000

    Local References

--- Components and Databinding/src/app/cockpit/cockpit.component.ts
+++ Components and Databinding/src/app/cockpit/cockpit.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 export class CockpitComponent implements OnInit {
 
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,16 +17,17 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
+
     this.serverCreated.emit({
-      serverName: this.newServerName, 
+      serverName: nameInput.value, 
       serverContent: this.newServerContent
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName, 
+      serverName: nameInput.value, 
       serverContent: this.newServerContent
     });
   }
