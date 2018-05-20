d0d33438706fffc95431f5e7616aa253fbc70f1a
jose-guerrero <jose.guerrerog@outlook.com>
2018-05-20 18:20:53 +1000

    Binding to Custom Events

--- Components and Databinding/src/app/app.component.ts
+++ Components and Databinding/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Server } from 'selenium-webdriver/safari';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
+  onServerAdded(ServerData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+      type: 'server',
+      name: ServerData.serverName,
+      content: ServerData.serverContent
+    }); 
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
   
 }
