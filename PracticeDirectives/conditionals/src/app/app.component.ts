a078b937b146f21c44ecd3252d8c534f332c6bbf
jose-guerrero <jose.guerrerog@outlook.com>
2018-05-27 13:04:04 +1000

    calling methods

--- PracticeDirectives/conditionals/src/app/app.component.ts
+++ PracticeDirectives/conditionals/src/app/app.component.ts
@@ -10,4 +10,43 @@ export class AppComponent {
   show: boolean = false;
   showing: boolean = false;
 
+  public myStyle = {
+    'color':'blue',
+    'background-color':'yellow'
+  };
+
+  public showText = false;
+  public switchOff(){
+    this.showText = !this.showText;
+  }
+  public TextColor = 'GREEN';
+
+  public ChangeTextColor(){
+
+    switch (this.TextColor){
+      case 'GREEN': {
+        this.TextColor = 'BLUE';
+        break;
+      }
+      case 'BLUE': {
+        this.TextColor = 'RED';
+        break;
+      }
+      case 'RED': {
+        this.TextColor = 'GREEN';
+        break;
+      }
+    }
+
+    /*
+    if (this.TextColor=='GREEN'){
+      this.TextColor = 'BLUE';
+    }
+    else{
+      if (this.TextColor=='BLUE')
+        this.TextColor = 'RED';
+      else
+        this.TextColor = 'GREEN';
+    }*/
+  }
 }
